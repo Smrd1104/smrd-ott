12add8f4ebff2a304b9b91ef704fc86938ee4e60
Smrd1104 <mriyasdeen1104@gmail.com>
2025-02-02 14:29:11 +0530

    changes

--- src/components/sports/Card.tsx
+++ src/components/sports/Card.tsx
@@ -1,12 +1,26 @@
-import img from '../../assets/the-boys.jpeg'
-const Card = () => {
-  return (
-    <div className=''>
-        <div className=''>
-        <img src={img} className='w-[300px] h-[250px] rounded-md border shadow-lg'/>
-        </div>
-    </div>
-  )
+interface CardProps {
+  title: string;
+  image: string;
 }
 
-export default Card
+const Card = ({ title, image }: CardProps) => {
+  return (
+    <div className="relative">
+      <img
+        src={image}
+        alt={title}
+        className="w-[300px] h-[250px] rounded-md border shadow-lg"
+      />
+      <div className="absolute bottom-3 left-3 bg-black/50 text-white p-2 rounded-md text-sm">
+        {title}
+        <div>
+     
+      </div>
+     
+      </div>
+    
+    </div>
+  );
+};
+
+export default Card;
--- src/components/sports/Sports.tsx
+++ src/components/sports/Sports.tsx
@@ -1,24 +1,36 @@
-import Card from "./Card"
+import Card from "./Card";
+import img1 from "../../assets/the-boys.jpeg";
+import img2 from "../../assets/maari.jpg";
+import img3 from "../../assets/the-boys.jpeg";
+import img4 from "../../assets/cricket.jpg";
+
+const sportsData = [
+  { id: 1, title: "The Boys", image: img1 },
+  { id: 2, title: "Maari", image: img2 },
+  { id: 3, title: "Tennis", image: img3 },
+  { id: 4, title: "Cricket", image: img4 },
+  { id: 5, title: "Baseball", image: img1 },
+  { id: 6, title: "Hockey", image: img2 },
+  { id: 7, title: "Golf", image: img3 },
+  { id: 8, title: "Volleyball", image: img4 },
+];
 
 const Sports = () => {
   return (
-    <div className=" my-10   flex flex-col gap-10 mx-10">
-     <div className="  grid md:grid-cols-4 grid-cols-2 gap-5 ">
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-     
-      </div>
-      <div className="  grid md:grid-cols-4 grid-cols-2 gap-5 ">
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-     
-      </div>
-    </div>
-  )
-}
+    <div className="my-10 mx-10 flex flex-col gap-10">
+      {/* Section Title */}
+      <h2 id="sports" className="text-2xl font-bold text-white">Sports Collection</h2>
 
-export default Sports
+      {/* Cards Grid */}
+      <div className="grid md:grid-cols-4 grid-cols-2 gap-5">
+        {sportsData.map((sport) => (
+          <Card key={sport.id} title={sport.title} image={sport.image} />
+        ))}
+       
+      </div>
+      
+    </div>
+  );
+};
+
+export default Sports;
