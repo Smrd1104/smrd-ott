61c7a3b212bb80d2138856cfbc9cb201a99a4dfe
Smrd1104 <mriyasdeen1104@gmail.com>
2025-02-04 15:03:20 +0530

    movies sectionupdated card

--- src/components/movies/Movies.tsx
+++ src/components/movies/Movies.tsx
@@ -1,30 +1,93 @@
 import { Swiper, SwiperSlide } from "swiper/react";
-import { IoIosArrowBack, IoIosArrowForward  } from "react-icons/io";
-
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 import "swiper/swiper-bundle.css";
 import { Navigation, Autoplay } from "swiper/modules";
 import Card from "./Card"; // Import your Card component
+// images
 import img1 from "../../assets/maari.jpg";
 import img2 from "../../assets/vettaiyan.jpg";
 import img3 from "../../assets/mangatha.jpg";
-import img4 from "../../assets/master.jpg";
-import img5 from "../../assets/goat.jpg";
+import img4 from "../../assets/goat.jpg";
+import img5 from "../../assets/master.jpg";
 import img6 from "../../assets/mei.jpg";
-import img7 from "../../assets/rrr.jpg";
-import img8 from "../../assets/spider.png";
+import img7 from "../../assets/pushpa.jpg";
+import img8 from "../../assets/rrr.jpg";
+// videos
+import bgVideo1 from "../../assets/bg-video-movie.mp4";
+import bgVideo2 from "../../assets/bg-1.mp4"
+import bgVideo3 from "../../assets/bg-2.mp4"
+import bgVideo4 from "../../assets/bg-3.mp4"
+import bgVideo5 from "../../assets/bg-4.mp4"
+import bgVideo6 from "../../assets/bg-5.mp4"
+import bgVideo7 from "../../assets/2bg.mp4"
+import bgVideo8 from "../../assets/4bg.mp4"
 
-const cards = [
-  { id: 1, title: "Maari", image: img1 },
-  { id: 2, title: "Vettaiyan", image: img2 },
-  { id: 3, title: "Mangatha", image: img3 },
-  { id: 4, title: "Master", image: img4 },
-  { id: 5, title: "Leo", image: img5 },
-  { id: 6, title: "Mei Azhlagan", image: img6 },
-  { id: 7, title: "RRR", image: img7 },
-  { id: 8, title: "Spider", image: img8 },
+
+
+
+const cardData = [
+  {
+    id: 1,
+    title: "Maari",
+    image: img1,
+    videoSrc: bgVideo1,
+    year: "2025 . U/A 2025 . 1 Season",
+    description: "Spider-free milestones are a mission",
+  },
+  {
+    id: 2,
+    title: "Vettaiyan",
+    image: img2,
+    videoSrc: bgVideo2,
+    year: "2024 . U/A 2024 . 1 Season",
+    description: "The hunt for justice in a crime-ridden",
+  },
+  {
+    id: 3,
+    title: "Mangatha",
+    image: img3,
+    videoSrc: bgVideo3,
+    year: "2025 . U/A 2025 . 1 Season",
+    description: "Spider-free milestones are a mission  ",
+  },{
+    id: 4,
+    title: "Leo",
+    image: img4,
+    videoSrc: bgVideo4,
+    year: "2025 . U/A 2025 . 1 Season",
+    description: "Spider-free milestones are a mission  ",
+  },{
+    id: 5,
+    title: "Master",
+    image: img5,
+    videoSrc: bgVideo5,
+    year: "2025 . U/A 2025 . 1 Season",
+    description: "Spider-free milestones are a mission  ",
+  },{
+    id: 6,
+    title: "Mei Azhlagan",
+    image: img6,
+    videoSrc: bgVideo6,
+    year: "2025 . U/A 2025 . 1 Season",
+    description: "Spider-free milestones are a mission  ",
+  },{
+    id: 7,
+    title: "Pushpa 2 ",
+    image: img7,
+    videoSrc: bgVideo7,
+    year: "2025 . U/A 2025 . 1 Season",
+    description: "Spider-free milestones are a mission  ",
+  },{
+    id: 8,
+    title: "RRR",
+    image: img8,
+    videoSrc: bgVideo8,
+    year: "2025 . U/A 2025 . 1 Season",
+    description: "Spider-free milestones are a mission  ",
+  },
 ];
 
-const movieSlider = () => {
+const MovieSlider = () => {
   return (
     <div id="movies" className="container mx-auto py-5 px-10 relative">
       <h1 className="md:text-[2.5rem] text-[2rem] my-5 drop-shadow-header font-bold">
@@ -32,12 +95,12 @@ const movieSlider = () => {
       </h1>
 
       {/* Custom Navigation Buttons */}
-      <IoIosArrowBack  
+      <IoIosArrowBack
         className="absolute top-1/2 translate-y-6 text-[3rem] left-6 z-10 text-white p-3 bg-black/20 rounded-full transition cursor-pointer"
         id="movie-prevSlide"
       />
-      
-      <IoIosArrowForward 
+
+      <IoIosArrowForward
         className="absolute top-1/2 translate-y-6 text-[3rem] right-6 z-10 text-white p-3 bg-black/20 rounded-full transition cursor-pointer"
         id="movie-nextSlide"
       />
@@ -49,7 +112,6 @@ const movieSlider = () => {
           nextEl: "#movie-nextSlide",
           prevEl: "#movie-prevSlide",
         }}
-        // autoplay={{ delay: 3000, disableOnInteraction: false }}
         loop={true}
         breakpoints={{
           320: { slidesPerView: 1 },
@@ -58,10 +120,16 @@ const movieSlider = () => {
           1280: { slidesPerView: 4 },
         }}
       >
-        {cards.map((card) => (
-          <SwiperSlide key={card.id}>
+        {cardData.map((item) => (
+          <SwiperSlide key={item.id}>
             <div className="hover:scale-105 duration-300 transition-all">
-              <Card title={card.title} image={card.image} />
+              <Card
+                title={item.title}
+                image={item.image}
+                videoSrc={item.videoSrc}
+                year={item.year}
+                description={item.description}
+              />
             </div>
           </SwiperSlide>
         ))}
@@ -70,4 +138,4 @@ const movieSlider = () => {
   );
 };
 
-export default movieSlider;
+export default MovieSlider;
--- src/components/movies/VideoCard.tsx
+++ src/components/movies/VideoCard.tsx
@@ -1,12 +1,17 @@
-import { useRef, useState } from 'react';
-import football from '../../assets/bg-video-movie.mp4';
-import { FaVolumeUp, FaVolumeMute } from 'react-icons/fa';
+import { useRef, useState } from "react";
+import { FaVolumeUp, FaVolumeMute } from "react-icons/fa";
 
-const VideoCard = () => {
-  const videoRef = useRef<HTMLVideoElement>(null); // Explicitly define the type as HTMLVideoElement
-  const [isMuted, setIsMuted] = useState(true); // State for mute/unmute
+interface VideoCardProps {
+  videoSrc: string;
+  title: string;
+  year: string;
+  description: string;
+}
 
-  // Function to handle mute/unmute toggle
+const VideoCard = ({ videoSrc, title, year, description }: VideoCardProps) => {
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const [isMuted, setIsMuted] = useState(true);
+
   const toggleMute = () => {
     if (videoRef.current) {
       videoRef.current.muted = !isMuted;
@@ -15,35 +20,33 @@ const VideoCard = () => {
   };
 
   return (
-    <div className="md:w-[250px] w-[350px] h-[280px] bg-black relative ">
+    <div className="md:w-[250px] w-[350px] h-[280px] bg-black relative">
       <video
         autoPlay
         ref={videoRef}
-        className="w-full h-1/2 rounded-md shadow-lg object-cover"
+        className="w-full h-1/2  shadow-lg object-cover"
         muted={isMuted}
       >
-        <source src={football} type="video/mp4" />
+        <source src={videoSrc} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
 
       {/* Custom volume control */}
-      <div className="absolute md:top-27 top-4  md:left-56 left-4 flex items-center gap-2">
+      <div className="absolute md:top-27 top-4 md:left-56 left-4 flex items-center gap-2">
         <button onClick={toggleMute} className="text-white cursor-pointer">
           {isMuted ? <FaVolumeMute size={15} /> : <FaVolumeUp size={15} />}
         </button>
       </div>
 
-      <div className="absolute bottom-0 left-0 right-0 flex flex-col gap-1 mt-2 pb-6 bg-white/10">
+      <div className="absolute bottom-0 left-0 right-0 flex flex-col gap-1 mt-2 pb-10 bg-white/10">
         <div className="mx-4">
           <button className="cursor-pointer border px-5 py-2 bg-white/10 rounded mt-1 text-white text-sm">
             Watch Now
           </button>
         </div>
-        <h1 className="text-sm text-white mx-4 mt-2">Maari</h1>
-        <h1 className="text-xs text-white mx-4">2025 . U/A 2025 . 1 Season</h1>
-        <p className="text-xs text-white mx-4">
-          Spider-free milestones are a mission to protect...
-        </p>
+        <h1 className="text-sm text-white mx-4 mt-2">{title}</h1>
+        <h1 className="text-xs text-white mx-4">{year}</h1>
+        <p className="text-xs text-white mx-4">{description}</p>
       </div>
     </div>
   );
