f62986dd74dacd80618a0c9525b535f8bc50597b
Smrd1104 <mriyasdeen1104@gmail.com>
2025-02-08 13:15:26 +0530

    changes

--- src/components/loader/Loader.tsx
+++ src/components/loader/Loader.tsx
@@ -7,12 +7,12 @@ const LoadingSpinner: React.FC = () => {
       <img 
         src={loader}// Ensure the correct path
         alt="Loading..."
-        className="w-28 h-28  z-50 absolute"
+        className="w-28 h-28 z-50 absolute"
       />
 
       {/* Animated Loader Background */}
       <div 
-        className="w-32 aspect-square rounded-full relative flex justify-center items-center animate-[spin_3s_linear_infinite] z-40 bg-[conic-gradient(transparent_0deg,transparent_300deg,transparent_270deg,transparent_360deg)]"
+        className="w-36 aspect-square rounded-full relative flex justify-center items-center animate-[spin_3s_linear_infinite] z-40 bg-[conic-gradient(transparent_0deg,transparent_300deg,transparent_270deg,transparent_360deg)]"
       >
         {/* Inner Animated Layer 1 */}
         {/* <div className="absolute w-[60%] aspect-square rounded-full z-[80] animate-[spin_2s_linear_infinite] bg-[conic-gradient(orange_0deg,orange_270deg,transparent_180deg,transparent_360deg)]"></div> */}
